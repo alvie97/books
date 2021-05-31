e0a94ce24cb014e8b7c4d8a35963117d4c905f70
alvie97 <alvie97m@gmail.com>
2021-05-31 00:18:51 -0300

    adds fron-page

---
+++ front-page.php
@@ -0,0 +1,63 @@
+<?php
+    get_header();
+?>
+
+<main class="flex flex-col items-center p-5">
+    <div id="books" class="inline-grid grid-cols-4 auto-rows-min justify-items-center gap-10">
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+
+        <!-- separator -->
+        
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+        <div class="flex flex-col w-52 space-y-1">
+            <img 
+                src="https://via.placeholder.com/400x640" 
+                 alt="book cover">
+            <a class="border-red-500 text-red-500 bg-red-100 border-solid border-2 px-2 py-1 text-center" href="#"> Download here </a>
+        </div>
+
+    </div>
+</main>
+
+<?php
+    get_footer();
